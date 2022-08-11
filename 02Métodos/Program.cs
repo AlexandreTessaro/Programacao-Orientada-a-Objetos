6f7f80a769d67de1a53f2fbd59b086700cbbe0a1
Alexandre Tessaro Vieira <alexandre.tassaro@gmail.com>
2022-08-11 17:49:16 -0300

    curso c#

--- 02Métodos/Program.cs
+++ 02Métodos/Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Pessoa obj = new Pessoa();
+obj.apresentar("Alexandre", 18);
---
+++ 03ExercíciosEscolar/Program.cs
@@ -0,0 +1,6 @@
+﻿// Instanciar objeto da classe Aluno
+Aluno a = new Aluno();
+a.nome = "Alexandre";
+a.nota1 = 8;
+a.nota2 = 10; 
+a.mensagem();
